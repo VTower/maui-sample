da3c90b3c390466bae71eaf2723e2de290db6a9c
Victor <signalpack326@gmail.com>
2025-08-05 09:40:59 -0300

    add EditCustomer

---
+++ Presentation/PopUp/EditCustomer/EditCustomerPopUp.xaml.cs
@@ -0,0 +1,40 @@
+using maui_sample.ViewModels;
+using CommunityToolkit.Maui.Views;
+using maui_sample.Models;
+
+namespace maui_sample.Presentation.PopUp;
+
+public partial class EditCustomerPopUp : Popup
+{
+    public EditCustomerPopUp(Client client)
+    {
+        InitializeComponent();
+
+        EditCustomerPopUpViewModel vm = new(client);
+        vm.OnSave += Close;
+        vm.OnCancel += () => Close();
+
+        BindingContext = vm;
+    }
+
+    // TODO: Faze injeção de dependencia
+    private void OnlyNumbers(object sender, TextChangedEventArgs e)
+    {
+        Entry entry = (Entry)sender;
+
+        string onlyCharNumbers = new(entry.Text?.Where(char.IsDigit).ToArray());
+
+        if (entry.Text != onlyCharNumbers)
+            entry.Text = onlyCharNumbers;
+    }
+
+    private void OnlyLetters(object sender, TextChangedEventArgs e)
+    {
+        Entry entry = (Entry)sender;
+
+        string onlyCharCaracter = new(entry.Text?.Where(char.IsLetter).ToArray());
+
+        if (entry.Text != onlyCharCaracter)
+            entry.Text = onlyCharCaracter;
+    }
+}
---
+++ Presentation/PopUp/EditCustomer/EditCustomerPopUpViewModel.cs
@@ -0,0 +1,93 @@
+using maui_sample.Models;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace maui_sample.ViewModels;
+
+public partial class EditCustomerPopUpViewModel : ObservableObject
+{
+    // TODO: Implemente this to avoid unnecessary behavior
+    bool _hasEdited = false;
+    private string _firstName;
+    public string FirstName
+    {
+        get => _firstName;
+        set
+        {
+            _hasEdited = true;
+            SetProperty(ref _firstName, value);
+        }
+    }
+    
+    private string _lastName;
+    public string LastName
+    {
+        get => _lastName;
+        set
+        {
+            _hasEdited = true;
+            SetProperty(ref _lastName, value);
+        }
+    }
+    
+    private int _age;
+    public int Age
+    {
+        get => _age;
+        set
+        {
+            _hasEdited = true;
+            SetProperty(ref _age, value);
+        }
+    }
+    
+    private string _address;
+    public string Address
+    {
+        get => _address;
+        set
+        {
+            _hasEdited = true;
+            SetProperty(ref _address, value);
+        }
+    }
+
+    // Commands 
+    public ICommand SaveCommand { get; }
+    public ICommand CloseCommand { get; }
+
+    // Handlers 
+    public event Action<Client>? OnSave;
+    public event Action? OnCancel;
+
+    public EditCustomerPopUpViewModel(Client client)
+    {
+        _firstName = client.FirstName;
+        _lastName = client.LastName;
+        _age = client.Age;
+        _address = client.Address;
+
+        // Update just if have changes
+        _hasEdited = false;
+
+        CloseCommand = new Command(() =>
+                      {
+                          // TODO: Add PopUp in main page to spawn PopUp Edit Customer cancelled 
+                          // Do Nothing..
+                          OnCancel?.Invoke();
+                      });
+
+        SaveCommand = new Command(() =>
+               {
+                   Client cliente = new()
+                   {
+                       FirstName = FirstName,
+                       LastName = LastName,
+                       Age = Age,
+                       Address = Address
+                   };
+
+                   OnSave?.Invoke(cliente);
+               });
+    }
+}
