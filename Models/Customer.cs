653a23abfaaa335570d585f4b926f37889d8ac6d
Victor <signalpack326@gmail.com>
2025-08-05 11:20:24 -0300

    Customer with Sqlite Label

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,10 +1,13 @@
+using SQLite;
+
 namespace maui_sample.Models;
 
 public record Customer
 {
-    public Guid   Id        { get; set; } = Guid.NewGuid();
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
     public string FirstName { get; set; } = string.Empty;
-    public string LastName  { get; set; } = string.Empty;
-    public int    Age       { get; set; }
-    public string Address   { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public int Age { get; set; }
+    public string Address { get; set; } = string.Empty;
 }
