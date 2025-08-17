a1eece69637a3c530b7fcaee5e665c3b30ec5f91
Victor <signalpack326@gmail.com>
2025-08-17 19:02:12 -0300

    Route Registration

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,16 @@
-﻿namespace maui_sample;
+﻿using maui_sample.ViewModels;
+
+namespace maui_sample;
 
 public partial class App : Application
 {
 	public App()
 	{
 		InitializeComponent();
+
+		// Rote App Register 
+		Routing.RegisterRoute(nameof(MainViewModel), typeof(MainPage));
+		Routing.RegisterRoute(nameof(BrownianViewModel), typeof(BrownianPage));
 	}
 
 	protected override Window CreateWindow(IActivationState? activationState)
